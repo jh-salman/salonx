1e7c604f9bfd3a39ae765333576ed08d19f6d9c9
jh-salman <jhsalman53@gmail.com>
2025-06-30 00:03:27 +0600

    add missing component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
-import Svg from '../component/svg'
-import Chart from '../component/Chart'
+
+i
 import TopStats from '../component/TopStats'
 import CompanyHeader from '../component/CompanyHeader'
 import ClientList from '../component/ClientList'
@@ -10,8 +9,7 @@ const Home = () => {
     <div className="relative flex flex-col items-center justify-center h-screen bg-gradient-to-br from-black to-gray-900">
       
       <div className="flex flex-col  items-center justify-center">
-        {/* <Svg /> */}
-        {/* <Chart /> */}
+     
         <TopStats />
         <CompanyHeader />
         <ClientList />
